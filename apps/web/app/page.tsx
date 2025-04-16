8f953c4456b28d1000e4886811606b667f9364ce
杨子文 <yangziwen1994@gmail.com>
2025-04-16 22:42:23 +0800

    feat(web): 添加登陆页面

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@repo/ui/components/button"
+import Link from "next/link"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Link href="/login">
+          <Button size="sm">Login</Button>
+        </Link>
       </div>
     </div>
   )
